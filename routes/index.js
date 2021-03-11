9d18e4de9e083238126101b2e3d436da796aa078
Sankar <433115@cognizant.com>
2021-03-11 10:02:56 +0530

    Predemo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Demo' });
+  res.render('index', { title: 'Pre Demo' });
 });
 
 module.exports = router;
